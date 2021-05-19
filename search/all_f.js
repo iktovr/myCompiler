cd87dcae97e20a3d69f463ad0d8388d6667a062b
iktovr <iktovr@users.noreply.github.com>
2021-05-19 10:26:04 +0000

    deploy: 8563ebca012afa23d67a4b703eb240362d2373b6

--- search/all_f.js
+++ search/all_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['t_70',['T',['../class_m_p_translator_1_1_grammar.html#ab5f7f51f45e962496e9042478a504744',1,'MPTranslator::Grammar']]],
-  ['tablekey_71',['Tablekey',['../struct_m_p_translator_1_1_tablekey.html',1,'MPTranslator']]],
+  ['tablekey_71',['Tablekey',['../struct_m_p_translator_1_1_canonical_l_r_parser_1_1_tablekey.html',1,'MPTranslator::CanonicalLRParser']]],
   ['terminals_72',['Terminals',['../class_m_p_translator_1_1_canonical_l_r_parser.html#ac897300a42f90d282f682321be489cf7',1,'MPTranslator::CanonicalLRParser']]],
   ['termreturn_73',['TermReturn',['../class_m_p_translator_1_1_grammar.html#a30f3d5f41e56318e7881990f71818eb3',1,'MPTranslator::Grammar']]],
   ['translator_74',['Translator',['../class_m_p_translator_1_1_translator.html',1,'MPTranslator.Translator'],['../class_m_p_translator_1_1_translator.html#a80a7708e05525bf3ffde9d43be8f3650',1,'MPTranslator.Translator.Translator()']]],
--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['tablekey_97',['Tablekey',['../struct_m_p_translator_1_1_tablekey.html',1,'MPTranslator']]],
+  ['tablekey_97',['Tablekey',['../struct_m_p_translator_1_1_canonical_l_r_parser_1_1_tablekey.html',1,'MPTranslator::CanonicalLRParser']]],
   ['translator_98',['Translator',['../class_m_p_translator_1_1_translator.html',1,'MPTranslator']]],
   ['translgrammar_99',['TranslGrammar',['../class_m_p_translator_1_1_transl_grammar.html',1,'MPTranslator']]],
   ['translmp_100',['translMp',['../class_m_p_translator_1_1transl_mp.html',1,'MPTranslator']]],
